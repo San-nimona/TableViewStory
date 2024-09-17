3527ec3ca422c1223a807826ad8d185bf207425a
San Nimona <kaybryanmc@gmail.com>
2024-09-17 15:52:57 +0300

    still

---
+++ TableViewStory/AttractionTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AttractionTableViewCell.swift
+//  TableViewStory
+//
+//  Created by katu on 11/09/2024.
+//
+
+import UIKit
+
+class AttractionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
